c73c8c59f728dbc75add2ad43e5b086d6e867d3f
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-07-01 07:06:41 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
 
-export class App extends Component {
+ class App extends Component {
   render() {
     return (
       <div className="App">
